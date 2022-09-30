f52fa8cc876a5c06294a3794935072e2cd03d4f3
Doston <gameplayer140701@gmail.com>
2022-09-30 14:44:19 +0500

    Типизация в TypeScript, НаÐназначение для пер Ð Ðтипов для Ð переменных

--- app.ts
+++ app.ts
@@ -1,1 +1,11 @@
-const a = 1;
+let revenue: number = 1000;
+let bonus: number =  500;
+let c: string = 'sdf';
+let d: boolean = true;
+// после названия переменной надо указать тип переменной что бы ТС строго типизировал наши переменные и не допускал ошибочных данных
+
+let res: number = revenue + bonus;
+
+console.log(res);
+//tsc - Компиляция ТС на JS
+//node app.js - Запус скрипта в терминале IDE
