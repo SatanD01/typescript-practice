3d8cced3a4d99921fc8763d61767344dab9f2810
Doston <gameplayer140701@gmail.com>
2022-09-30 15:43:03 +0500

    Tuples here we can show howMany types can be in array and which of type can be in array

--- app.ts
+++ app.ts
@@ -1,12 +1,17 @@
-const skills: string[] = ['Dev', 'DevOps', 'Testing']
-// Когда мы передаем перенной массив надо и его типизировать пример как здесь принимает массив строк и мы не можем здесь передать ничего кроме строк
+const skill: [number, string] = [1, 'Dev'];
+// Использование Tuples тоесть ограниценный массив в нем может хранится только указанное
+// Количество массив типов тоесть [number, string] = [1, 'Dev']
 
-for (const skill of skills) {
-    console.log(skill);
-}
+const [id, skillName] = skill;
 
-const res = skills.filter((s: string) => s !== 'DevOps').map(s => s + '! ').reduce((a, b) => a + b);
+// const id = skill[0];
+// const skillName = skill[1];
 
-console.log(res)
+skill.push('test');
+// Даже если мы через push добавим элемент в массив мы не сможем кнему обратиться skill[2]
+
+const arr: [number, string, ...boolean[]] = [1, 'test', false, false, true];
+
+
 //tsc - Компиляция ТС на JS
 //node app.js - Запус скрипта в терминале IDE
