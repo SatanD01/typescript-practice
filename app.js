ff34ece45c8dc6193f8b1d2e760bd75a66763149
Doston <gameplayer140701@gmail.com>
2022-09-30 14:58:24 +0500

    types in Functions in TS and check typeof incoming data for types

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 "use strict";
-let revenue = 1000;
-let bonus = 500;
-let res = revenue + bonus;
-console.log(res);
+function getFullName(firstName, lastName) {
+    // if (typeof firstName !== "string") {
+    //     throw new Error('!!!!');
+    // //   Если из вне приходят даные то надо использовать такую проверку на типы в другиз случаях не обязательно использовать проверку на типы
+    // }
+    return `${firstName} ${lastName}`;
+}
+const getFullNameArrow = (firstName, lastName) => {
+    return `${firstName} ${lastName}`;
+};
+console.log(getFullName('Anton', 'Johan'));
+//tsc - Компиляция ТС на JS
+//node app.js - Запус скрипта в терминале IDE
--- app.ts
+++ app.ts
@@ -1,11 +1,17 @@
-let revenue: number = 1000;
-let bonus: number =  500;
-let c: string = 'sdf';
-let d: boolean = true;
-// после названия переменной надо указать тип переменной что бы ТС строго типизировал наши переменные и не допускал ошибочных данных
+function getFullName(firstName: string, lastName: string) {
+    // if (typeof firstName !== "string") {
+    //     throw new Error('!!!!');
+    // //   Если из вне приходят даные то надо использовать такую проверку на типы в другиз случаях не обязательно использовать проверку на типы
+    // }
 
-let res: number = revenue + bonus;
+    return `${firstName} ${lastName}`
+}
 
-console.log(res);
+const getFullNameArrow = (firstName: string, lastName: string) => {
+    return `${firstName} ${lastName}`
+}
+
+
+console.log(getFullName('Anton', 'Johan'));
 //tsc - Компиляция ТС на JS
 //node app.js - Запус скрипта в терминале IDE
